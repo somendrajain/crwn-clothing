5a282bcc1a9a959a3ee1f6ce1abe9328f6378ef4
unknown <somendrajain1799@gmail.com>
2021-06-13 21:15:44 +0530

    implemented firebase utils

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,7 +11,34 @@ const config = {
     appId: "1:612742583047:web:affdb1f62dcbdb81062ba2",
     measurementId: "G-3L05TC4F9F"
   };
+
+  
   firebase.initializeApp(config);
+  
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
 
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
