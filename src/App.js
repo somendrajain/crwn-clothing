f461fb02ee6138f060a10b0defedc3ab9c040f27
unknown <somendrajain1799@gmail.com>
2021-06-15 12:22:53 +0530

    Redirect to homepage if user is already signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import './App.css';
 import ShopPage from './pages/shop/shop.component'
 import HomePage from './pages/homepage/homepage.component';
@@ -42,7 +42,15 @@ componentWillUnmount() {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
-          <Route exact path='/signin' component={SignInSignUpPage} />
+          <Route exact path='/signin' 
+              render={() =>
+                this.props.currentUser ? (
+                  <Redirect to='/' />
+                ) : (
+                  <SignInSignUpPage />
+                )
+              }
+          />
         </Switch>
       </div>
       );
@@ -50,8 +58,12 @@ componentWillUnmount() {
   
 }
 
-const mapDispatchToprops = dispatch => ({
+const mapStateToProps = ({ user}) => ({
+  currentUser:user.currentUser
+})
+
+const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null,mapDispatchToprops)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
